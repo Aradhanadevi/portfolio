c745aab4bbcef6b6b6ca9d361aa6a21b8d3c3cc1
Aradhanadevi <aradhanajadeja81@gmail.com>
2024-04-19 15:17:10 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import styles from './App.module.css'
 import { About } from './components/About/About';
+import { Experiance } from './components/Experiance/Experiance';
 import { Hero } from './components/Hero/Hero';
 import { Navbar } from './components/Navbar/Navbar';
 
@@ -8,6 +9,7 @@ function App() {
       <Navbar/>
       <Hero />
       <About/>
+      <Experiance/>
     </div>;
   
 }
