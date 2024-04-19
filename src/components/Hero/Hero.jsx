745aa2130853628ff4cdb1bc35a1704aa000914f
Aradhanadevi <aradhanajadeja81@gmail.com>
2024-04-20 00:40:59 +0530

    updates

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,14 +6,14 @@ export const Hero = () => {
   return (
   <section className={styles.container}>
     <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Devi</h1>
+        <h1 className={styles.title}>Aradhana</h1>
         <p className={styles.description}>
             I am a web developer, also I like making 3D modles in 
             blender, and sometimes I also make games.
         </p>
         <a href="mailto:aradhanajadeja81@gmail.com" className={styles.contactBtn}>Contact Me</a>
     </div>
-    <img src={getImageUrl("hero/heroImage.png")} alt="My Image" className={styles.heroImg} />
+    <img src={getImageUrl("hero/try.jpeg")} alt="My Image" className={styles.heroImg} />
     <div className={styles.topBlur} />
     <div className={styles.bottomBlur} />
   </section>
