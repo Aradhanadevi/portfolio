0534bd1392b3f4ef8ae637bfd4169eb5eeff03ad
Aradhanadevi <aradhanajadeja81@gmail.com>
2024-04-19 18:21:04 +0530

    project

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { About } from './components/About/About';
 import { Experiance } from './components/Experiance/Experiance';
 import { Hero } from './components/Hero/Hero';
 import { Navbar } from './components/Navbar/Navbar';
+import { Projects } from './components/Projects/Projects';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <About/>
       <Experiance/>
+      <Projects/>
     </div>
     );
 }
